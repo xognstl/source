090a6e6b269d2fa431d2964c104c91eca68e8d17
xognstl <xognstl@naver.com>
2025-08-12 09:19:40 +0900

    20250812

---
+++ data-jpa/src/main/java/study/data_jpa/controller/MemberController.java
@@ -0,0 +1,52 @@
+package study.data_jpa.controller;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import study.data_jpa.dto.MemberDto;
+import study.data_jpa.entity.Member;
+import study.data_jpa.repository.MemberRepository;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Long id) {
+        Member member = memberRepository.findById(id).get();
+        return member.getUsername();
+    }
+
+    // 도메인 클래스 컨버터
+    // HTTP 파라미터로 넘어온 엔티티의 아이디로 엔티티 객체를 찾아서 바인딩
+    @GetMapping("/members2/{id}")
+    public String findMember2(@PathVariable("id") Member member) {
+        return member.getUsername();
+    }
+
+    // 페이징과 정렬
+    // http://localhost:8080/members?page=0&size=10&sort=id,desc&sort=username,desc
+    // @PageableDefault 로 설정 가능 yml보다 이게 우선
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size = 5, sort = "username", direction = Sort.Direction.DESC) Pageable pageable, Sort sort) {
+        Page<Member> page = memberRepository.findAll(pageable);
+        Page<MemberDto> map = page.map(member -> new MemberDto(member.getId(), member.getUsername(), null));
+        return map;
+    }
+
+
+    @PostConstruct
+    public void init() {
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user" + i, i));
+        }
+    }
+}
